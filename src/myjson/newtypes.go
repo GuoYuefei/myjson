8acf850c85b372b3bad884f2f579f16d4a2c9ed8
Solomon Guo <beconegood@gmail.com>
2018-11-12 23:44:01 +0800

    调整了文件，内容

--- src/myjson/newtypes.go
+++ src/myjson/newtypes.go
@@ -1,5 +1,7 @@
 package myjson
 
+
+//----------这个是jsonValue.go需要定义的简单结构---------------
 type NumberType int
 
 const(
@@ -19,3 +21,33 @@ type JsObject = map[string]*Value
 
 //有一个点可以确定，切片中的数据必须是Value类型的，这是一个递归的结构
 type Slice = []*Value
+
+
+//----------这个是token.go需要定义的数据-------------------------
+//忽然返现定义token前需要先定义状态
+type Status int
+
+const(
+	//已经绝望的状态  啥都不期待了
+	StaNone Status = iota
+	//期待key
+	StaKey
+	//期待Value
+	StaValue
+	//期待大括号
+	StaCloBrace
+	//期待双引号
+	StaQuotation
+	//期待中括号
+	StaSquare
+
+)
+
+//这个是包内全局变量，用于记录“\"”是否被期待，因为这个部分前后，想{}[]是不同字符成对出现的
+//外部禁止访问，这样会导致不安全
+var flagQuo = false
+
+
+
+
+
--- src/myjson/token.go
+++ src/myjson/token.go
@@ -4,28 +4,6 @@ package myjson
 //先定义其数据结构，因为其实json是树形结构的，所以token其实有点像树的节点
 
 
-//忽然返现定义token前需要先定义状态
-type Status int
-
-const(
-	//已经绝望的状态  啥都不期待了
-	StaNone Status = iota
-	//期待key
-	StaKey
-	//期待Value
-	StaValue
-	//期待大括号
-	StaCloBrace
-	//期待双引号
-	StaQuotation
-	//期待中括号
-	StaSquare
-
-)
-
-//这个是包内全局变量，用于记录“\"”是否被期待，因为这个部分前后，想{}[]是不同字符成对出现的
-//外部禁止访问，这样会导致不安全
-var flagQuo = false
 type Sign struct {
 	//标识符一般是有自己的期待值的，比如说”{“是会期待”}“。 ","是期待key的出现
 	status Status
@@ -68,20 +46,25 @@ type Token struct {
 	//前节点，这是一个双亲表示法
 	parent *Token
 	//当前节点的值
-	value Value
+	value *Value
 	//当前节点的状态，这个状态取决于前一个关键字也就是前一个Sign对象，特别注意{ “ ： ，[
 	status Status
 }
 
+func NewToken(p *Token, v *Value, s Status) *Token {
+	return &Token{p, v, s}
+}
 
 func (t *Token)GetParent() *Token {
 	return t.parent
 }
 
 func (t *Token)GetValue() *Value {
-	return &t.value
+	return t.value
 }
 
 func (t *Token)GetStatus() Status {
 	return t.status
 }
+
+//还需要一个栈，这个栈需要自定义一些方法，所以感觉可以重开一个文件
